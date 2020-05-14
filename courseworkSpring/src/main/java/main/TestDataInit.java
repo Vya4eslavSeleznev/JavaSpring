67a714beb5194c6a93fdbe74d0fd2b5e10f07ce3
Vya4eslav111 <vya4eslav.seleznev@gmail.com>
2020-05-14 22:28:33 +0300

    Spring security

---
+++ courseworkSpring/src/main/java/main/TestDataInit.java
@@ -0,0 +1,32 @@
+package main;
+
+import main.entity.User;
+import main.repository.OperationRepository;
+import main.repository.UserRepository;
+import org.postgresql.util.PSQLException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+
+@Component
+public class TestDataInit implements CommandLineRunner
+{
+  @Autowired
+  OperationRepository appRep;
+
+  @Autowired
+  UserRepository userRep;
+
+  @Autowired
+  PasswordEncoder pwdEncoder;
+
+  @Override
+  public void run (String... args) throws Exception
+  {
+    //userRep.save(new User("user", pwdEncoder.encode("pwd"), Collections.singletonList("ROLE_USER")));
+    //userRep.save(new User("admin", pwdEncoder.encode("apwd"), Collections.singletonList("ROLE_ADMIN")));
+  }
+}
---
+++ courseworkSpring/src/main/java/main/exception/InvalidJwtAuthenticationException.java
@@ -0,0 +1,9 @@
+package main.exception;
+
+public class InvalidJwtAuthenticationException extends RuntimeException
+{
+  public InvalidJwtAuthenticationException(String message)
+  {
+    super(message);
+  }
+}
