30f79781a65098f43319c3865be9bbc026dcdb13
Vya4eslav111 <vya4eslav.seleznev@gmail.com>
2020-05-17 23:31:07 +0300

    Authentication form

--- UI/src/coursework/Authentication.java
+++ UI/src/coursework/Authentication.java
@@ -1,5 +1,22 @@
 package coursework;
 
+import javax.swing.*;
+
 public class Authentication
 {
+  public Authentication()
+  {
+    JFrame frame = new JFrame();
+    frame.add(rootPanel);
+    frame.setTitle("Home budget");
+    frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    frame.setSize(1000, 700);
+    frame.setLocation(290, 50);
+    frame.setVisible(true);
+  }
+
+  private JPanel rootPanel;
+  private JPasswordField passwordField;
+  private JTextField textField;
+  private JButton signInButton;
 }
